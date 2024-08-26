9541413cd78c2049a9b6f959df9463a50e705c5d
MICHELLE SOFIA GARBER <48521612@ort>
2024-08-26 08:21:47 -0300

    creando clases

---
+++ Models/Categorias.cs
@@ -0,0 +1,5 @@
+public class Categorias{
+public int IdCategoria {get; set;}
+public string Nombre {get; set;}
+public string Foto {get; set;}
+}
---
+++ Models/Juego.cs
@@ -0,0 +1,21 @@
+public class Juego{
+//Atributos
+private static string username {get; set;}
+private static int puntajeActual {get; set;}
+private static int cantidadPreguntasCorrectas {get; set;}
+private static List<Preguntas> preguntas {get; set;}
+private static List<Respuestas> preguntas1 {get; set;}
+
+
+//Metodos
+public static void InicializarJuego(){
+    username = string.Empty;
+    puntajeActual = 0;
+    cantidadPreguntasCorrectas = 0;
+}
+
+public static List<Categorias> InicializarJuego(){
+    
+}
+}
+
